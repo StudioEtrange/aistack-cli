fd7e12269752a6413a0f877eef25cf71fbda999a
StudioEtrange <nomorgan@gmail.com>
2026-03-15 02:24:21 +0100

    first work on orla agent

---
+++ lib/main_orla.sh
@@ -0,0 +1,86 @@
+if ! check_requirements "yq"; then echo " -- ERROR : yq missing, launch aistack init"; exit 1; fi;
+local sub_command="$1"
+shift
+case "$sub_command" in
+    install)
+
+        echo "Installing Orla"
+        orla_install "latest"
+        
+        echo "Configuring Orla"
+        orla_settings_configure
+
+        orla_launcher_manage
+        ;;
+    uninstall)
+        # clean running process
+        process_kill_by_port "8081" 1>/dev/null 2>&1
+        
+        echo "Uninstalling Orla (keeping all configuration unchanged. to remove configuration use reset command)"
+        orla_uninstall
+
+        orla_launcher_manage "delete"
+        ;;
+    configure)
+        echo "Configuring Orla"
+        orla_settings_configure
+
+        orla_launcher_manage
+        ;;
+    info)
+        orla_info
+        ;;
+    show-config)
+        if [ -f "$AISTACK_ORLA_CONFIG_FILE" ]; then
+            echo "Current Orla configuration file : $AISTACK_ORLA_CONFIG_FILE"
+            cat "$AISTACK_ORLA_CONFIG_FILE"
+        else
+            echo "No Orla configuration file found."
+        fi
+        ;;
+    set)
+        case "$3" in
+            "string")
+                orla_set_config "$1" "$2" "double"
+                ;;
+            *)
+                orla_set_config "$1" "$2"
+                ;;
+        esac
+        ;;
+    get)
+        orla_get_config "$1"
+        ;;
+    reset)
+        echo "Resetting Orla configuration"
+        orla_settings_remove
+
+        orla_launcher_manage
+        ;;
+   
+    launch)
+        orla_launcher_manage
+
+        local folder=
+        if [ -n "$1" ] && [ "$1" != "--" ]; then
+            folder="$1"
+            if [ -d "$folder" ]; then
+                echo "change to context folder : $folder"
+                cd "$folder" || exit 1
+                shift
+            else
+                echo "Error: Directory '$folder' not found"
+                exit 1
+            fi
+        fi
+        [ "$1" = "--" ] && shift
+
+        orla_launch "$@"
+        ;;
+    
+    *)
+        echo "Error: Unknown command $sub_command for Orla"
+        usage
+        exit 1
+        ;;
+esac
